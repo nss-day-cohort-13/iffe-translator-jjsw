0f054587e85b817505c9ad703c78d149a5a63e32
Sulaiman.Allen <xalerons@gmail.com>
2016-04-27 11:17:09 -0500

    Added a ton of comments + removed console logs and fixed errors.

--- italian.js
+++ italian.js
@@ -1,23 +1,16 @@
 
 var wordBank = (function(origObj) {
-	console.log("Inside wordBank italy", typeof(origObj));
+
 	var italianWords = {
 		"happy":"felice",
 		"new":"nuovo",
 		"year":"anno"};
 
 	origObj.getItalian = function(item) {
-		// return origObj.italianWords[item];
-		// console.log("inside of getItalian fuction. Item = ", item);
-		// console.log("item = ", item);
-
 		// go through each word in the dictionary and compare it against the current word.
 		for (key in italianWords) { 
-			// console.log(italianWords[key]);
+
 			if (key === item) {
-				// console.log("Key found key ===", key)
-				// console.log("item = ", item);
-				console.log("test", italianWords[item]);
 				return italianWords[item];
 			}
 		}
