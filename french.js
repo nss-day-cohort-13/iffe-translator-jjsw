4a48b9384bab58ec923ed7b74fd1a6177c60ef87
Sulaiman.Allen <xalerons@gmail.com>
2016-04-28 10:30:25 -0500

    Readded french file

---
+++ french.js
@@ -0,0 +1,41 @@
+var wordBank = (function(origObj) {
+	console.log("Inside wordBank france", typeof(origObj));
+
+	var frenchTrans = [];
+	var frenchWords = {
+		"happy":"bonne",
+		"new":"nouvelle",
+		"year":"année",
+    "thanksgiving":"action de grâces", 
+    "congratulations":"félicitations à vous",
+    "merry" : "joyeux",
+    "christmas" : "Noël",
+    "hannukah" : "Hannukah",
+    "holidays" : "vacances",
+    "birthday" : "anniversaire",   
+    "the" : "la",
+    "and" : "et",
+    "of" : "de",
+    "season" : "saison",
+    "good" : "bien",
+    "tidings" : "nouvelles",
+    "festivus" : "festivus",
+    "for" : "pour",
+    "a" : "une",
+    "rest" : "le repos",
+    "us" : "nous",
+    "graduation" : "l'obtention du diplôme"
+    };
+
+	origObj.getFrench = function(item) {
+    // console.log("French, item = ", item);
+
+    // console.log("frenchWords[item] = ", frenchWords[item]);
+          return frenchWords[item];
+        //  }
+        //  return "VOID";
+        };
+
+        return origObj;
+
+    })(wordBank);
